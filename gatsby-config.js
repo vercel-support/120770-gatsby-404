9f9a645cab10e7b853349e43a90c46d6b1c3214c
William Li <email@williamli.dev>
2023-03-07 13:01:49 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `120770-gatsby-404`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
